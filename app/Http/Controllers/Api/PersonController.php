6c1d8e6a70c8de2b9e4a2a7ac0ed1d08c744e139
jacquelyn bloomfield <jbloomfield16@lapu.edu>
2024-03-11 15:43:09 +0000

    migrations and shit

---
+++ app/Http/Controllers/Api/PersonController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Person;
+use Illuminate\Http\Request;
+
+class PersonController extends Controller
+{
+    public function index()
+    {
+        $people = Person::all();
+        // ->with('aliases');
+        return response()->json($people);
+    }
+}
--- resources/js/Navigation.jsx
+++ resources/js/Navigation.jsx
@@ -1,37 +1,29 @@
 import '/resources/scss/Navigation.scss'
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Navigation() {
+    const [isMenuVisible, setIsMenuVisible] = useState(true);
 
-    const button = document.querySelector('.left-menu__visibility-toggle')
-    const navPanel = document.querySelector('.left-menu')
-
-    button.addEventListener('click', () => {
-        navPanel.classList.toggle('left-menu_hidden')
-        button.textContent = button.textContent == '>' ? '<' : '>'
-    })
-
+    const toggleMenuVisibility = () => {
+        setIsMenuVisible(!isMenuVisible);
+    };
 
     return (
-
-        <nav className="left-menu">
-
-            <div className="left-menu__visibility-toggle">&#60;</div>
+        <nav className={`left-menu ${isMenuVisible ? '' : 'left-menu_hidden'}`}>
+            <div className="left-menu__visibility-toggle" onClick={toggleMenuVisibility}>
+                {isMenuVisible ? '<' : '>'}
+            </div>
 
             <div className="left-menu__content">
-
                 <div className="left-menu__header">
                     <img className="left-menu__seal" src="https://classes.codingbootcamp.cz/assets/classes/1404/mi6-seal.png" alt="MI6 seal" />
                 </div>
 
                 <div className="left-menu__links">
-                    <a to="/">Home</a>
-                    <a to="/people-of-interest">People of interest</a>
+                    <a href="/">Home</a> 
+                    <a href="/people-of-interest">People of interest</a> 
                 </div>
             </div>
-
         </nav>
-
-    )
-
+    );
 }
